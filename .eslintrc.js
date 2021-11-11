10119edbaf5bfccad94dea5688d88f491a1fda62
mcesarpl <mcesarpl123@gmail.com>
2021-11-10 21:32:40 -0300

    eslint e jest config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   env: {
     es2021: true,
     node: true,
+    jest: true,
   },
-  extends: ["airbnb-base"],
-  parser: "@typescript-eslint/parser",
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {},
 };
---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.path, { prefix: '<rootDir>' }),
+  preset: 'ts-jest',
+};
